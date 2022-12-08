6619aae071f485c89694f4391b99bdeb9b94d2e2
Cristhian Uriel <a3521110258@alumno.uttehuacan.edu.mx>
2022-12-08 00:45:37 -0600

    contador

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  counter :number=0;
+//databinding
+
+  constructor() {  }
+  incrementar(){
+    console.log("El boton ha sido presionado")
+    this.counter++;
+    
+  }
+  reset(){
+    this.counter=0;
+  }
+
+}
